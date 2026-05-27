9de75a48f3ea8dfbe529bd664f846a2abb65a765
Mir-37 <mirmutasim37@gmail.com>
2026-05-28 04:10:06 +0600

    feat(ui): implement category selection navigation
    Add navigation logic to `CategoriesScreen` to allow users to navigate
    to the `MealsScreen` when a category is selected. This involves
    updating `CategoryGridItem` to accept an `onSelectCategory` callback
    and implementing the `_selectCategory` method in `CategoriesScreen`.

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:khuda_lagse/data/dummy_data.dart';
+import 'package:khuda_lagse/screens/meals_screen.dart';
 import 'package:khuda_lagse/widget/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(
+    BuildContext context,
+  ) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(meals: [], title: 'some title'),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +35,12 @@ class CategoriesScreen extends StatelessWidget {
         ),
         children: [
           for (final category in availableCategories)
-            CategoryGridItem(category: category),
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context);
+              },
+            ),
         ],
       ),
     );
