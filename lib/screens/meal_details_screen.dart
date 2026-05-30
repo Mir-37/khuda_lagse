ad7a379d2aaf118d6966f933d2390100b6ef1727
Mir-37 <mirmutasim37@gmail.com>
2026-05-30 18:53:52 +0600

    feat(ui): update meal details to reflect favorite status
    Integrate `favoriteMealsProvider` in `MealDeatailsScreen` to dynamically
    toggle the star icon between filled and outlined states based on the
    meal's favorite status.

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -13,6 +13,10 @@ class MealDeatailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
@@ -34,7 +38,7 @@ class MealDeatailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(Icons.star),
+            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
           ),
         ],
       ),
